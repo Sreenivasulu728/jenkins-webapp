991c811a82457f561546f8a2e7a009d3af069d46
Sreenivasulu728 <127183716+Sreenivasulu728@users.noreply.github.com>
2024-06-28 16:09:44 +0530

    Update App.java

--- src/main/java/com/sample/java/App.java
+++ src/main/java/com/sample/java/App.java
@@ -1,13 +1,13 @@
 package com.sample.java;
 
 /**
- * Hello world!
+ * Hello world from jenkins-github!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World from jenkins-github!" );
     }
 }
